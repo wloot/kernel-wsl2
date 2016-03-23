a5cb48e622133f2a90d1a915652229efd6147e78
Arjan van de Ven <arjan@linux.intel.com>
2016-03-23 14:52:41 +0000

    init_task: faster timerslack
    the default tuning is a compromise between client power and server
    performance;
    
    for a server distro like Clear Linux, we don't need to compromise.
    (for non-server usages we have different kernel binaries)
    
    in principle this can be done as a patch to systemd as well, but we have
    a shared systemd between usages while we have different kernels, so the
    logistics for where the patch goes work out better here

--- init/init_task.c
+++ init/init_task.c
@@ -117,7 +117,7 @@ struct task_struct init_task
 	.journal_info	= NULL,
 	INIT_CPU_TIMERS(init_task)
 	.pi_lock	= __RAW_SPIN_LOCK_UNLOCKED(init_task.pi_lock),
-	.timer_slack_ns = 50000, /* 50 usec default slack */
+	.timer_slack_ns =  1000, /*  1 usec default slack */
 	.thread_pid	= &init_struct_pid,
 	.thread_group	= LIST_HEAD_INIT(init_task.thread_group),
 	.thread_node	= LIST_HEAD_INIT(init_signals.thread_head),
